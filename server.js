e694fc73aad176c29d73e51261bdc8f2f13578da
Jonas <skolekonto47@gmail.com>
2023-03-15 14:00:00 +0100

    added database connection

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 
 const app = express();
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	database: 'test'
+  });
+
+  connection.connect(function(err) {
+	if(err) throw(err);
+	console.log("Connected!");
+  });
 
 app.get('/', (req, res) => {
 	res.send('<h1>My node App</h1>');
