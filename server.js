32a499dc378f0fb4d7c66796b8404343dec104d9
Jesper Kjærgaard <jezper@hotmail.dk>
2023-03-21 17:28:30 +0100

    psuh

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const fs = require('fs');
 
 const app = express();
 
@@ -19,6 +20,8 @@ const connection = mysql.createConnection({
   });
 
 app.get('/usersite', (req, res) => {
+
+
 	res.render('usersite');
 });
 
@@ -34,17 +37,39 @@ app.get('/createrecipes', (req, res) => {
 	res.render('createRecipes');
 });
 
-app.get('/recapie/:recapieID', (req, res) => {
-    let recapieId = req.url.split('/')[2]
+app.get('/recapie/:recapieID?', (req, res) => {
+    const recapieId = req.params.recapieID
+    const queryRecipe = 'recipes.title, recipes.instructions, recipes.personorstk, recipes.totalAmount, recipes.dateCreated, recipes.foodImg'
+    const queryIngredients = 'ingredients.ingredient, ingredients.measuringUnit, ingredients.amount'
 
-	connection.query('SELECT * FROM recipes WHERE id = ?', [recapieId], (err, recapiData) => {
+    // Henter opskrift, ingredienser og forfatter  fra databasen   
+	const query = ` SELECT ${queryRecipe}, users.fullName, ${queryIngredients}
+            FROM recipes
+            INNER JOIN users ON recipes.userId = users.id
+            INNER JOIN ingredients ON recipes.id = ingredients.recipeId
+            WHERE recipes.id = ?`
+
+    connection.query(query, [recapieId], (err, data) => {
         if (err) {
             console.log(err)
         } else {
-                let userId = recapiData[0].userId
-                connection.query('SELECT fullName FROM users WHERE id= ?', [userId], (err, userData) => {
-                res.render('recipieSite', {userName:userData[0].fullName, recapiData: recapiData[0]});
-            })
+            // Henter kommentarende fra databasen
+            const query = `SELECT comments.userComment, comments.stars, users.fullName
+            FROM comments
+            INNER JOIN users ON comments.userId = users.id 
+            WHERE comments.recipeId = ?`
+
+            connection.query(query, [recapieId], (err, comments) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    let date = data[0].dateCreated 
+                    date = (new Date(date)).toISOString().slice(0,10)
+        
+                    // Går til siden og sender data man kan bruger på frontenden
+                    res.render('recipieSite', {data: data[0], array: data, date: date, comments: comments});
+                }
+            })            
         }
     })
 });
